a2486f0c6f0737e68334fb260ebc255dcfb956b6
Bogdan Kokunin <bkokunin@gmail.com>
2025-03-12 01:19:03 +0300

    fix: try different approach with routing

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
+  ssr: false,
+  future: {
+    v7_partialHydration: true,
+  },
   basename:
-    process.env.NODE_ENV === "production" ? "/outliers-visualization" : "/",
+    process.env.NODE_ENV === "production" ? "/outliers-visualization/" : "/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/outliers-visualization/",
+  base:
+    process.env.NODE_ENV === "production" ? "/outliers-visualization/" : "/",
 });
