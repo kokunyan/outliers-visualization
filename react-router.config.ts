9f88bccefd6acf41b07af55860b0af82cdede24c
Bogdan Kokunin <bkokunin@gmail.com>
2025-03-12 01:13:09 +0300

    feat: configure github pages deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,4 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  basename:
+    process.env.NODE_ENV === "production" ? "/outliers-visualization" : "/",
 } satisfies Config;
